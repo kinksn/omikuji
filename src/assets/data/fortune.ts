128a77eb2903d5487a1274b09fca0cc6cb1af25c
kinksn <kataoka0417@gmail.com>
2024-01-17 11:57:01 +0900

    test

--- src/assets/data/fortune.ts
+++ src/assets/data/fortune.ts
@@ -1,3 +1,11 @@
+import Daikichi from '/public/result/daikichi.png';
+import Chukichi from '/public/result/chukichi.png';
+import Kichi from '/public/result/kichi.png';
+import Shokichi from '/public/result/syokichi.png';
+import Suekichi from '/public/result/suekichi.png';
+import Kyo from '/public/result/kyo.png';
+import Daikyo from '/public/result/daikyo.png';
+
 export type Fortune = {
   name: string;
   image: string;
@@ -5,11 +13,11 @@ export type Fortune = {
 };
 
 export const fortunes: Fortune[] = [
-  { name: '大吉', image: '/public/result/daikichi.png', weight: 10 },
-  { name: '中吉', image: '/public/result/chukichi.png', weight: 30 },
-  { name: '吉', image: '/public/result/kichi.png', weight: 50 },
-  { name: '小吉', image: '/public/result/suekichi.png', weight: 70 },
-  { name: '末吉', image: '/public/result/suekichi.png', weight: 80 },
-  { name: '凶', image: '/public/result/kyo.png', weight: 15 },
-  { name: '大凶', image: '/public/result/daikyo.png', weight: 5 }
+  { name: '大吉', image: Daikichi, weight: 10 },
+  { name: '中吉', image: Chukichi, weight: 30 },
+  { name: '吉', image: Kichi, weight: 50 },
+  { name: '小吉', image: Shokichi, weight: 70 },
+  { name: '末吉', image: Suekichi, weight: 80 },
+  { name: '凶', image: Kyo, weight: 15 },
+  { name: '大凶', image: Daikyo, weight: 5 }
 ];
