c7f34d3f3d219fd24071686d4fe4a65f1cd92580
kinksn <kataoka0417@gmail.com>
2024-01-20 10:40:18 +0900

    確率調整

--- src/assets/data/fortune.ts
+++ src/assets/data/fortune.ts
@@ -13,11 +13,11 @@ export type Fortune = {
 };
 
 export const fortunes: Fortune[] = [
-  { name: '大吉', image: Daikichi, weight: 10 },
+  { name: '大吉', image: Daikichi, weight: 25 },
   { name: '中吉', image: Chukichi, weight: 30 },
   { name: '吉', image: Kichi, weight: 50 },
-  { name: '小吉', image: Shokichi, weight: 70 },
-  { name: '末吉', image: Suekichi, weight: 80 },
-  { name: '凶', image: Kyo, weight: 15 },
+  { name: '小吉', image: Shokichi, weight: 60 },
+  { name: '末吉', image: Suekichi, weight: 70 },
+  { name: '凶', image: Kyo, weight: 10 },
   { name: '大凶', image: Daikyo, weight: 5 }
 ];
